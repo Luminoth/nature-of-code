7a1dc15d7793a23ab221276f3fef54b5ec7a32bf
Shane <ignignokterr@gmail.com>
2021-04-24 20:00:04 -0700

    Drag

--- mover-example/src/main.rs
+++ mover-example/src/main.rs
@@ -7,12 +7,48 @@ use processing::Screen;
 use rand::Rng;
 
 #[derive(Debug, Default)]
+struct Liquid {
+    location: Vector2<f64>,
+    size: Vector2<f64>,
+    c: f64,
+}
+
+impl Liquid {
+    fn new(x: f64, y: f64, w: f64, h: f64, c: f64) -> Self {
+        Self {
+            location: Vector2::new(x, y),
+            size: Vector2::new(w, h),
+            c,
+        }
+    }
+
+    fn contains(&self, mover: &Mover) -> bool {
+        mover.location.x > self.location.x
+            && mover.location.x < self.location.x + self.size.x
+            && mover.location.y > self.location.y
+            && mover.location.y < self.location.y + self.size.y
+    }
+
+    fn display(&self, screen: &mut Screen) -> Result<(), ProcessingErr> {
+        screen.stroke_off();
+        core::fill_grayscale(screen, 175.0);
+
+        core::shapes::rect(
+            screen,
+            self.location.x,
+            self.location.y,
+            self.size.x,
+            self.size.y,
+        )
+    }
+}
+
+#[derive(Debug, Default)]
 struct Mover {
     location: Vector2<f64>,
     velocity: Vector2<f64>,
     acceleration: Vector2<f64>,
     mass: f64,
-    //topspeed: f64,
 }
 
 impl Mover {
@@ -20,7 +56,6 @@ impl Mover {
         Self {
             location: Vector2::new(x, y),
             mass,
-            //topspeed: 1.0,
             ..Default::default()
         }
     }
@@ -75,6 +110,10 @@ impl Mover {
     }
 
     fn apply_force(&mut self, force: Vector2<f64>) {
+        if force.x.is_nan() || force.y.is_nan() {
+            return;
+        }
+
         let force = force / self.mass;
         self.acceleration += force;
     }
@@ -88,8 +127,15 @@ impl Mover {
         );
     }
 
+    fn drag(&mut self, liquid: &Liquid) {
+        let speed = self.velocity.magnitude();
+        let drag_magnitude = liquid.c * speed * speed;
+
+        let drag = (self.velocity * -1.0).normalize() * drag_magnitude;
+        self.apply_force(drag);
+    }
+
     fn update(&mut self) {
-        //self.velocity = (self.velocity + self.acceleration).cap_magnitude(self.topspeed);
         self.velocity += self.acceleration;
         self.location += self.velocity;
 
@@ -114,21 +160,29 @@ fn setup<'a>() -> Result<Screen<'a>, ProcessingErr> {
     core::create_canvas(640, 360)
 }
 
-fn draw(screen: &mut Screen, movers: &mut impl AsMut<[Mover]>) -> Result<(), ProcessingErr> {
+fn draw(
+    screen: &mut Screen,
+    movers: &mut impl AsMut<[Mover]>,
+    liquid: &Liquid,
+) -> Result<(), ProcessingErr> {
     core::background_grayscale(screen, 255.0);
 
-    let wind = Vector2::new(0.01, 0.0);
+    liquid.display(screen)?;
+
+    //let wind = Vector2::new(0.01, 0.0);
     let gravity = Vector2::new(0.0, 0.1);
 
-    let c = 0.01;
+    //let c = 0.01;
 
     for mover in movers.as_mut() {
-        let friction = (mover.velocity * -1.0).normalize() * c;
-        if !friction.x.is_nan() && !friction.y.is_nan() {
-            mover.apply_force(friction);
+        /*let friction = (mover.velocity * -1.0).normalize() * c;
+        mover.apply_force(friction);*/
+
+        if liquid.contains(mover) {
+            mover.drag(liquid);
         }
 
-        mover.apply_force(wind);
+        //mover.apply_force(wind);
         mover.apply_force(gravity * mover.mass);
 
         mover.update();
@@ -141,6 +195,7 @@ fn draw(screen: &mut Screen, movers: &mut impl AsMut<[Mover]>) -> Result<(), Pro
 
 fn main() -> Result<(), ProcessingErr> {
     let movers = Rc::new(RefCell::new(None));
+    let liquid = Rc::new(RefCell::new(None));
 
     core::run(
         || {
@@ -151,14 +206,28 @@ fn main() -> Result<(), ProcessingErr> {
             let mut mvrs = vec![];
             for _ in 0..100 {
                 let x = rng.gen_range(0..screen.width()) as f64;
-                let y = rng.gen_range(0..screen.height()) as f64;
+                let y = rng.gen_range(0..screen.height() / 4) as f64;
                 mvrs.push(Mover::new(rng.gen_range(0.1..5.0), x, y));
             }
             *movers.borrow_mut() = Some(mvrs);
 
+            *liquid.borrow_mut() = Some(Liquid::new(
+                0.0,
+                screen.height() as f64 / 2.0,
+                screen.width() as f64,
+                screen.height() as f64 / 2.0,
+                0.1,
+            ));
+
             Ok(screen)
         },
-        |screen, _| draw(screen, movers.borrow_mut().as_mut().unwrap()),
+        |screen, _| {
+            draw(
+                screen,
+                movers.borrow_mut().as_mut().unwrap(),
+                liquid.borrow().as_ref().unwrap(),
+            )
+        },
     )?;
 
     Ok(())
