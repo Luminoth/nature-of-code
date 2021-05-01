bf177513f8f908d9b94655434763221904e14b67
Shane <ignignokterr@gmail.com>
2021-05-01 14:02:55 -0700

    Doc

--- ecosystem/src/util.rs
+++ ecosystem/src/util.rs
@@ -4,6 +4,8 @@
 
 use bevy::prelude::*;
 
+/// Gets the 2-argument arctangent of the vector
+/// Useful for getting the "heading" angle of a vector
 pub fn atan2(vec: Vec2) -> f32 {
     vec.x.atan2(vec.y)
 }
