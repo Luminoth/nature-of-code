6e50b18f3978978783da7b5c9fbd6dc055436e19
Shane Lillie <ignignokterr@gmail.com>
2021-05-08 20:15:30 -0700

    Particle system

--- particle-system/src/main.rs
+++ particle-system/src/main.rs
@@ -16,11 +16,11 @@ struct Particle {
 }
 
 impl Particle {
-    fn new(x: f64, y: f64) -> Self {
+    fn new(location: DVec2) -> Self {
         let mut rng = rand::thread_rng();
 
         Self {
-            location: DVec2::new(x, y),
+            location,
             acceleration: DVec2::new(0.0, 0.05),
             velocity: DVec2::new(rng.gen_range(-1.0..1.0), rng.gen_range(-2.0..0.0)),
             lifespan: 255.0,
@@ -55,47 +55,76 @@ impl Particle {
     }
 }
 
+#[derive(Debug, Default)]
+struct ParticleSystem {
+    origin: DVec2,
+    particles: Vec<Particle>,
+}
+
+impl ParticleSystem {
+    fn new(x: f64, y: f64) -> Self {
+        Self {
+            origin: DVec2::new(x, y),
+            ..Default::default()
+        }
+    }
+
+    fn add_particle(&mut self) {
+        self.particles.push(Particle::new(self.origin));
+    }
+
+    fn run(&mut self, screen: &mut Screen, dt: f64) -> Result<(), ProcessingErr> {
+        self.add_particle();
+
+        // drain_filter() equivalent
+        let mut i = 0;
+        while i != self.particles.len() {
+            let particle = &mut self.particles[i];
+            particle.run(screen, dt)?;
+
+            if particle.is_dead() {
+                self.particles.remove(i);
+            } else {
+                i += 1;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 fn setup<'a>() -> Result<Screen<'a>, ProcessingErr> {
     core::create_canvas(640, 360)
 }
 
-fn draw(screen: &mut Screen, dt: f64, particles: &mut Vec<Particle>) -> Result<(), ProcessingErr> {
+fn draw(
+    screen: &mut Screen,
+    dt: f64,
+    particle_system: &mut ParticleSystem,
+) -> Result<(), ProcessingErr> {
     core::background_grayscale(screen, 255.0);
 
-    particles.push(Particle::new(screen.width() as f64 / 2.0, 50.0));
-
-    // drain_filter() equivalent
-    let mut i = 0;
-    while i != particles.len() {
-        let particle = &mut particles[i];
-        particle.run(screen, dt)?;
-
-        if particle.is_dead() {
-            particles.remove(i);
-        } else {
-            i += 1;
-        }
-    }
+    particle_system.run(screen, dt)?;
 
     Ok(())
 }
 
 fn main() -> Result<(), ProcessingErr> {
-    let particles = Rc::new(RefCell::new(None));
+    let particle_system = Rc::new(RefCell::new(None));
 
     core::run(
         || {
             let screen = setup()?;
 
-            let mut p = vec![];
+            let mut p = ParticleSystem::new(screen.width() as f64 / 2.0, 50.0);
             for _ in 0..10 {
-                p.push(Particle::default());
+                p.add_particle();
             }
-            *particles.borrow_mut() = Some(p);
+            *particle_system.borrow_mut() = Some(p);
 
             Ok(screen)
         },
-        |screen, dt| draw(screen, dt, particles.borrow_mut().as_mut().unwrap()),
+        |screen, dt| draw(screen, dt, particle_system.borrow_mut().as_mut().unwrap()),
     )?;
 
     Ok(())
